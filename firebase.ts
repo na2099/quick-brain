039aafb0b8dcc8a73407f9c2f728da504c4de702
Nimi <takinroy@ualberta.ca>
2024-08-14 14:01:34 -0600

    chore: Add Clerk and Firebase dependencies, update layout

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXAhvORQhIgpSCgx9h3NtEEXvdrLeGzB0",
+  authDomain: "quickbrain-a11eb.firebaseapp.com",
+  projectId: "quickbrain-a11eb",
+  storageBucket: "quickbrain-a11eb.appspot.com",
+  messagingSenderId: "164610906017",
+  appId: "1:164610906017:web:eafedc2df518d22eecdde0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
