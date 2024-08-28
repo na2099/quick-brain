232bf37c175d4a93243060d9f689971ccf6cbbde
Muhammed Nabeel Azard <nabeelazard@cmail.carleton.ca>
2024-08-27 21:50:03 -0400

    Added createContext to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@/components/Navbar';
-import React from 'react';
+import React, { createContext } from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
 import Features from './components/Features';
